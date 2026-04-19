9b74c344e8188029b31bf12f5525d693cb7c334d
Yasho9797 <shoam9797@Gemail.com>
2026-04-20 01:51:31 +0300

    the fainal

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-
-
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -21,7 +18,6 @@ import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      {/* PersistGate דואג שהאפליקציה תחכה לטעינת הנתונים מהזיכרון */}
       <PersistGate loading={null} persistor={persistor}> 
           <BrowserRouter>
               <App />
